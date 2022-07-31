2ad98e6395658fab0972ddefde691b95c4b8e0d1
imkarimkarim <allalirezamail@gmail.com>
2022-07-31 17:14:32 +0430

    in the name of bug

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+const convertToEnDigits = (num: string): string | undefined => {
+  if (!num) return;
+  num = num.toString();
+  if (!num) return num;
+  num = num
+    .replace(/۰/g, "0")
+    .replace(/۱/g, "1")
+    .replace(/۲/g, "2")
+    .replace(/۳/g, "3")
+    .replace(/۴/g, "4")
+    .replace(/۵/g, "5")
+    .replace(/۶/g, "6")
+    .replace(/۷/g, "7")
+    .replace(/۸/g, "8")
+    .replace(/۹/g, "9");
+  return num;
+};
+
+export default convertToEnDigits;
